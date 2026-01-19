e59179c5d10bcb40dc7996f7009593918f691269
Nazar Melnik <115361939+Arlikey@users.noreply.github.com>
2026-01-19 15:36:13 +0200

    develop: installed react router, added icons as react components, plugin prettier-tailwind, set up font rules

---
+++ baza-r/src/core/components/icons/Cross.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Cross = () => {
+  return (
+    <svg
+      width="10"
+      height="10"
+      viewBox="0 0 10 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.5 0.5L8.9375 8.9375"
+        stroke="#898888"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M0.5 8.9375L8.9375 0.5"
+        stroke="#898888"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Cross;
