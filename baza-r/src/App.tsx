d4ebbcefd11126ddbffd4ecb3e4efc0a59fe13cd
Nazar Melnik <115361939+Arlikey@users.noreply.github.com>
2026-01-19 12:00:49 +0200

    main: installed tailwind css, set up color scheme

--- baza-r/src/App.tsx
+++ baza-r/src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css'
-
 function App() {
   return (
     <>
+    <div className="text-secondary">hello everybody</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
